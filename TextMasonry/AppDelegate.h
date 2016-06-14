f30606e935b52df04c97d22fd53891f9ddef1701
chenyi <490901377@qq.com>
2016-06-15 07:48:49 +0800

    Initial Commit

---
+++ TextMasonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextMasonry
+//
+//  Created by Apple on 16/6/15.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
